aaa1189ccfed52a1fce3988cd485c92bf3034710
shankusu <shankusu2017@gmail.com>
2024-05-11 15:03:06 +0800

    little

--- url.go
+++ url.go
@@ -8,3 +8,7 @@ const (
 	URL_EVENT_POST      = "/v1/event/post"          // 事件上报的 URL
 	URL_REPEATER_SERVER = "/v1/vpn/repeater/server" // REPEATER SERVER 信息
 )
+
+const (
+	PORT_REPEATER_SERVER = 465
+)
