c6548307fa92f192fe8f1a9469b11a83620f3752
shankusu <shankusu2017@gmail.com>
2024-05-11 14:19:30 +0800

    little

--- url.go
+++ url.go
@@ -1,6 +1,10 @@
 package url
 
 const (
+	DOMAIN_MGR = "mgr.shanks.link" // 主机管理
+)
+
+const (
 	URL_EVENT_POST      = "/v1/event/post"          // 事件上报的 URL
 	URL_REPEATER_SERVER = "/v1/vpn/repeater/server" // REPEATER SERVER 信息
 )
