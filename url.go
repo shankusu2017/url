3f1069757acaa523f66fb62e7f76e3562d7e495a
shankusu <shankusu2017@gmail.com>
2024-05-20 14:36:00 +0800

    add: URL_EVENT_GET

--- url.go
+++ url.go
@@ -5,12 +5,13 @@ const (
 )
 
 const (
-	URL_EVENT_POST      = "/v1/event/post"          // 事件上报的 URL
-	URL_REPEATER_SERVER = "/v1/vpn/repeater/server" // REPEATER SERVER 信息
+	URL_EVENT_POST      = "/v1/event/post"          /* 事件上报的 URL */
+	URL_EVENT_GET       = "/v1/event/get"           /* 查看事件 */
+	URL_REPEATER_SERVER = "/v1/vpn/repeater/server" /* REPEATER SERVER 信息 */
 )
 
 const (
 	PORT_REPEATER_SERVER        = 465
-	PORT_REPEATER_SERVER_IPERF3 = 5201 // 假装成 iperf3 的数据包
+	PORT_REPEATER_SERVER_IPERF3 = 5201 /* 假装成 iperf3 的数据包 */
 	PORT_NODEMGR                = 7080
 )
